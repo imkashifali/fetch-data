ff9473cd0ada4dd633e2d7839070405e546850b9
Your Name <shafatkashif@gmail.com>
2020-09-24 05:26:11 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState  } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [repos, setrepos] = useState([{}]);
+
+  useEffect( () => {
+
+     //SECOND Method for Get Api
+    async function fetchGetApi(){
+      const response = await fetch("https://api.github.com/users/kashifshafati/repos");
+      const data = await response.json();
+      console.log(data);
+      setrepos(data)
+    }
+    fetchGetApi()
+   
+
+      //First Method for Get Api
+    //   fetch('https://jsonplaceholder.typicode.com/todos/1')
+    //   .then(response => response.json())
+    //   .then(json => {
+    //     setData(json)})
+  }, []);
+
+  return <div className="App">
+    <h1>You Are Seening All Repositry</h1>
+    {repos.map((repoObj,i) => {
+      return(<li key={i}>
+        {repoObj.name}
+        {repoObj.full_name}
+       
+      
+      </li>)
+    })}
+    </div>;
 }
 
 export default App;
